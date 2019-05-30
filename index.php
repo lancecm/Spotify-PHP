fa10bd0a14a11d86b37180a01659b266d4f0f939
MingLiu <mingl19@uci.edu>
2019-05-30 01:14:51 -0700

    volumn bar

--- index.php
+++ index.php
@@ -84,7 +84,17 @@
             </div>
 
             <div id="nowPlayingRight">
+                <div class="volumeBar">
+                    <button class="controlButton volume" title="volume button">
+                        <img src="assets/images/icons/volume.png" alt="Volume">
+                    </button>
 
+                    <div class="progressBar">
+                        <div class="progressBarBg">
+                            <div class="progress"></div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
