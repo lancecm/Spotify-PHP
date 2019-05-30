5050022a81454e98e0bfc319c0136e1d39f84b70
MingLiu <mingl19@uci.edu>
2019-05-29 23:18:00 -0700

    add buttons

--- index.php
+++ index.php
@@ -22,9 +22,43 @@
             <div id="nowPlayingLeft">
 
             </div>
+
             <div id="nowPlayingCenter">
 
+                <div class="content playerControls">
+
+                    <div class="buttons">
+
+                        <button class="controlButton shuffle" title="shuffle button">
+                            <img src="assets/images/icons/shuffle.png" alt="shuffle">
+                        </button>
+
+                        <button class="controlButton previous" title="previous button">
+                            <img src="assets/images/icons/previous.png" alt="previous">
+                        </button>
+
+                        <button class="controlButton play" title="play button">
+                            <img src="assets/images/icons/play.png" alt="play">
+                        </button>
+
+                        <button class="controlButton pause" title="pause button" style="display: none">
+                            <img src="assets/images/icons/pause.png" alt="pause">
+                        </button>
+
+                        <button class="controlButton next" title="next button">
+                            <img src="assets/images/icons/next.png" alt="next">
+                        </button>
+
+                        <button class="controlButton repeat" title="repeat button">
+                            <img src="assets/images/icons/repeat.png" alt="repeat">
+                        </button>
+
+                    </div>
+                    
+                </div>
+
             </div>
+
             <div id="nowPlayingRight">
 
             </div>
