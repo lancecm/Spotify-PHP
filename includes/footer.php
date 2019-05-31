efcea1d814196d43522cbb2ee325c435bdeba5ac
MingLiu <mingl19@uci.edu>
2019-05-31 01:12:41 -0700

    header & footer split

---
+++ includes/footer.php
@@ -0,0 +1,11 @@
+            </div>
+        </div>
+    </div>
+
+    <div id="nowPlayingBarContainer">
+        <?php include("includes/nowPlayingBar.php"); ?>
+    </div>
+</div>
+
+</body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,25 @@
+<?php
+include("includes/config.php");
+
+// Log out manually
+//    session_destroy();
+
+if (isset($_SESSION['userLoggedIn'])) {
+    $userLoggedIn = $_SESSION['userLoggedIn'];
+} else {
+    header("Location: register.php");
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    <title>test</title>
+</head>
+<body>
+<div id="mainContainer">
+
+    <div id="topContainer">
+        <?php include("includes/navBarContainer.php"); ?>
+        <div id="mainViewContainer">
+            <div id="mainContent">
