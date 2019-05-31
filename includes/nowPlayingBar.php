43efe41cd2b4163551d6859f932d9b97a44d3df5
MingLiu <mingl19@uci.edu>
2019-05-31 00:31:03 -0700

    split components to different page

---
+++ includes/nowPlayingBar.php
@@ -0,0 +1,79 @@
+<div id="nowPlayingBar">
+
+    <div id="nowPlayingLeft">
+        <div class="content">
+                    <span class="albumLink">
+                        <img class="albumArtwork" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgF7mH7yaynjt7b-BnPXnqhCfD9kM4j3lyzkzk1fp2j9ZCJaTJ">
+                    </span>
+
+            <div class="trackInfo">
+                        <span class="trackName">
+                            <span>gotinko</span>
+                        </span>
+                <span class="artistName">
+                            <span>ming</span>
+                        </span>
+            </div>
+        </div>
+    </div>
+
+    <div id="nowPlayingCenter">
+
+        <div class="content playerControls">
+
+            <div class="buttons">
+
+                <button class="controlButton shuffle" title="shuffle button">
+                    <img src="assets/images/icons/shuffle.png" alt="shuffle">
+                </button>
+
+                <button class="controlButton previous" title="previous button">
+                    <img src="assets/images/icons/previous.png" alt="previous">
+                </button>
+
+                <button class="controlButton play" title="play button">
+                    <img src="assets/images/icons/play.png" alt="play">
+                </button>
+
+                <button class="controlButton pause" title="pause button" style="display: none">
+                    <img src="assets/images/icons/pause.png" alt="pause">
+                </button>
+
+                <button class="controlButton next" title="next button">
+                    <img src="assets/images/icons/next.png" alt="next">
+                </button>
+
+                <button class="controlButton repeat" title="repeat button">
+                    <img src="assets/images/icons/repeat.png" alt="repeat">
+                </button>
+
+            </div>
+
+            <div class="playbackBar">
+                <span class="progressTime current">0:00</span>
+                <div class="progressBar">
+                    <div class="progressBarBg">
+                        <div class="progress"></div>
+                    </div>
+                </div>
+                <span class="progressTime remaining">0:00</span>
+            </div>
+
+        </div>
+
+    </div>
+
+    <div id="nowPlayingRight">
+        <div class="volumeBar">
+            <button class="controlButton volume" title="volume button">
+                <img src="assets/images/icons/volume.png" alt="Volume">
+            </button>
+
+            <div class="progressBar">
+                <div class="progressBarBg">
+                    <div class="progress"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
