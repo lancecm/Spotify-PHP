eae80970fea1bd4c2a126c2b11fb8df51826960b
MingLiu <mingl19@uci.edu>
2019-06-02 02:32:28 -0700

    add track count

--- album.php
+++ album.php
@@ -25,9 +25,20 @@
 <div class="trackListContainer">
     <ul class="trackList">
         <?php
+
             $songIdArray = $album->getSongIds();
+            $i = 1;
             foreach ($songIdArray as $songId) {
-                echo $songId."<br>";
+
+                $albumSong = new Song($con, $songId);
+                $albumArtist = $albumSong->getArtist();
+                echo "<li class='trackListRow'>
+                        <div class='trackCount'>
+                            <img class='play' src='assets/images/icons/play-white.png'>
+                            <span class='trackNumber'>i</span>
+                        </div>
+                      </li>";
+                $i++;
             }
         ?>
     </ul>
